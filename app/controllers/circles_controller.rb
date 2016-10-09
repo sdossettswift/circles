91734930f1027b6a3514c08f29a490a99352ac8b
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-09 17:53:59 -0500

    controllers for circles, events, people

---
+++ app/controllers/circles_controller.rb
@@ -0,0 +1,19 @@
+class CirclesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,19 @@
+class EventsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
