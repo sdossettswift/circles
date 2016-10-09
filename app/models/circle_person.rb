382fb452d2b92d559360aa23f867c00189210eb1
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-10-09 16:52:03 -0500

    removed tests, recreated circleperson

---
+++ app/models/circle_person.rb
@@ -0,0 +1,2 @@
+class CirclePerson < ApplicationRecord
+end
--- test/models/people_circle_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe PeopleCircle do
-  let(:people_circle) { PeopleCircle.new }
-
-  it "must be valid" do
-    value(people_circle).must_be :valid?
-  end
-end
